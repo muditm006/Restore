f3d55b34aef66d6cad9325f5d6781e4fbfa18f5d
muditm006 <88556020+muditm006@users.noreply.github.com>
2021-08-21 12:57:24 -0400

    Added user input to customize the timer time

--- script.js
+++ script.js
@@ -1,20 +1,27 @@
-const startingMinutes=10;
-let time= startingMinutes*60;
-
 const coundownEl= document.querySelector("#countdown")
+var time=0;
+var a=0;
 
-setInterval(updateCountdown, 1000);
 
 function updateCountdown()
 {
-	const minutes= Math.floor(time/60);
+	const minutes= Math.floor(time/600);
 	let seconds= time%60;
 	seconds= seconds<10 ? '0' + seconds: seconds;
+	if(seconds+minutes==0)
+	{
+		clearInterval(a);
+	}
 	coundownEl.innerHTML= `${minutes}:${seconds}`;
 	time--;
 }
 
 function submit()
 {
-	
+	clearInterval(a);
+	mins = document.querySelector("#min");
+	secs = document.querySelector("#sec");
+	time= mins.value+secs.value/60;
+	time= time*60;
+	a= setInterval(updateCountdown, 1000);
 }
